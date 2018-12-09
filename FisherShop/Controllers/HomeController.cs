2b68a4370ce52d20817ab0900a7a4d78bf497195
oldman28 <35926988+oldman28@users.noreply.github.com>
2018-12-10 01:02:51 +0400

    first commit

---
+++ FisherShop/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FisherShop.Models;
+
+namespace FisherShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            DataBaseDataContext db = new DataBaseDataContext();
+            List <Items> itemList = db.Items.ToList();
+            return View(itemList);
+        }
+    }
+}
